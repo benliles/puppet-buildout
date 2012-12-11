3e2a1801d58bfcce4868cd1bd3278c4cbdd0833d
Benjamin Liles <benliles@arch.tamu.edu>
2012-12-10 22:34:53 -0600

    Whitespace cleanup, bug fixes for redo

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,87 +1,94 @@
-# Class: buildout
+# Definition: buildout
 #
-# This module manages buildout and provides a define for setting up buildout
-# environments.
+# setup buildout python environment
+#
+# Parameters:
+#   $source  - source from which to grab buildout.cfg
+#   $path    - relative path for buildout configuration file
+#   $python  - the python interpreter to use
+#   $ensure  - flag to setup or remove the buildout environment
+#
+# Actions:
+#   setup buildout virtual python environment
+#
+# Requires:
+#   $source must be set
+#
+# Sample Usage:
+#
+#    buildout { "/path/to/buildout_env":
+#        source => "puppet:///files/mybuildout.cfg",
+#        python => "/path/to/your/python",
+#    }
 #
 class buildout {
 
-    package { "python": }
+  package { "python": }
 
-    # Definition: buildout::venv
-    #
-    # setup buildout virtual python environment
-    #
-    # Parameters:
-    #   $source  - source from which to grab buildout.cfg
-    #   $path    - relative path for buildout configuration file
-    #   $python  - the python interpreter to use
-    #   $ensure  - flag to setup or remove the buildout environment
-    #
-    # Actions:
-    #   setup buildout virtual python environment
-    #
-    # Requires:
-    #   $source must be set
-    #
-    # Sample Usage:
-    #
-    #    buildout::venv { "/path/to/buildout_env":
-    #        source => "puppet:///files/mybuildout.cfg",
-    #        python => "/path/to/your/python",
-    #    }
-    #
-    define venv($source=false, $path=false, $owner=false, $group=false, $python='/usr/bin/python', $ensure=present) {
-        if $path {
-            $config = "$name/buildout.cfg"
-        } else {
-            $config = "$name/buildout.cfg"
-        }
+  define venv (
+    $source=false,
+    $path="buildout.cfg",
+    $owner=false,
+    $group=false,
+    $python='/usr/bin/python',
+    $ensure=present
+  ) {
+    if $ensure == present {
+      $config = "${name}/${path}"
 
-        if $owner != false {
-            File { owner => $owner }
-        }
-        if $group != false {
-            File { group => $group }
-        }
+      if $owner != false {
+        File { owner => $owner }
+      }
+      if $group != false {
+        File { group => $group }
+      }
 
-        if $ensure == present {
-            file { $name:
-                ensure => directory
-            }
-            
-            file { "$name/bootstrap.py":
-                source => "puppet:///modules/buildout/bootstrap.py",
-                require => File[$name],
-            }
-            if $source {
-              file { $config:
-                source  => $source,
-                require => File[$name]
-              }
-            }
-            else {
-                file { $config:
-                    require => File[$name],
-                }
-            }
-            exec { "${python} $name/bootstrap.py":
-                cwd     => $name,
-                require => [File["$name/bootstrap.py"], File[$config]],
-                creates => "$name/bin/buildout",
-            }
-            exec { "$name/bin/buildout -c $config":
-                cwd => $name,
-                require => [
-                    Exec["${python} $name/bootstrap.py"],
-                    File[$config]],
-                subscribe => File[$config],
-                refreshonly => true,
-            }
-        } else {
-            file { "$name":
-                ensure => absent,
-                force => true,
-            }
+      file { "${name}":
+        ensure => directory
+      }
+
+      file { "${name}/bootstrap.py":
+        source => "puppet:///modules/buildout/bootstrap.py",
+        require => File["${name}"],
+      }
+
+      if $source {
+        file { $config:
+          source  => $source,
+          require => File["${name}"]
         }
+      }
+      else {
+        file { $config:
+          require => File["${name}"],
+        }
+      }
+
+      exec { "${python} ${name}/bootstrap.py":
+        cwd         => "${name}",
+        creates     => "${name}/bin/buildout",
+        require     => [
+          File["${name}/bootstrap.py"],
+          File[$config],
+        ],
+      }
+      exec { "${name}/bin/buildout -c ${config}":
+        cwd => "${name}",
+        refreshonly => true,
+        subscribe => [
+          File[$config],
+          Exec["${python} ${name}/bootstrap.py"],
+        ],
+        require => [
+          File[$config],
+          Exec["${python} ${name}/bootstrap.py"],
+        ],
+      }
+    } else {
+      file { "${name}":
+        ensure => absent,
+        force => true,
+      }
     }
+  }
 }
